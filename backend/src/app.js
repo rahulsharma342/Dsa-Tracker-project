05bb8b34743d0469464ae30b770ce0ad22b9518f
rahulsharma342 <rahulkv247@gmail.com>
2026-03-23 17:57:10 +0530

    final update 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,16 +11,18 @@ const app = express();
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  "http://localhost:5173",
-  "http://localhost:5174",
-].filter(Boolean);
+];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
